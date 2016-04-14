c0233c1725e2d6f46886405269d1fb882f420284
ThomasJohnson <tjohnson1106@gmail.com>
2016-04-14 10:54:56 -0400

    added event handler button

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,16 @@
  import React from 'react';
+ import Chance from 'chance';
 
  class Detail extends React.Component {
- 	render() {
- 		return <p > This is JSX being converted to HTML!< /p>;
+ 	buttonClicked() {
+ 		console.log('Button was clicked!')
  	}
- }
+ render() {
+    return (<div>
+        <p>Hello, {chance.first()}.</p>
+        <p>You are from {chance.country({ full: true })}.</p>
+        <button onClick={this.buttonClicked}>Meet Someone New</button>
+    </div>);
+}
 
  export default Detail;
