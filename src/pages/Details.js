fced1e0481f3c2cb086d0d84d7f3bd817eaabcdb
ThomasJohnson <tjohnson1106@gmail.com>
2016-04-15 14:50:33 -0400

    added new rendering method to convert Github JSON to JSX

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,31 +1,29 @@
  import React from 'react';
- import Chance from 'chance';
+
 
  class Detail extends React.Component {
- 		buttonClicked() {
- 			console.log('Button was clicked!')
- 		}
- 		constructor(props) {
-    super(props);
 
-    const people = [];
+ 	constructor(props) {
+ 		super(props);
 
-    for (let i = 0; i < 10; i++) {
-        people.push({
-            name: chance.first(),
-            country: chance.country({ full: true })
-        });
-    }
+ 		this.state = {
+ 			commits: []
+ 		};
+ 	}
+ }
 
-    this.state = { people };
-}
-}
-
-render() {
-    return (<div>
-    {this.state.people.map((person, index) => (
-        <p>Hello, {person.name} from {person.country}!</p>
-    ))}
-    </div>);
-
+ render() {
+ 		render() {
+ 				return ( < div > {
+ 						this.state.people.map((person, index) => ( < p key = {
+ 								index
+ 							} > Hello, {
+ 								person.name
+ 							}
+ 							from {
+ 								person.country
+ 							}! < /p>
+ 						))
+ 					} < /div>);
+ 				}
  				export default Detail;
